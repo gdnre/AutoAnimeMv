b0ecbb97890f1943b449e40247fd568694bd21e5
gdnre <94197241+gdnre@users.noreply.github.com>
2023-11-09 02:19:22 +0800

    添加剔除意外字符的规则

--- AutoAnimeMv.py
+++ AutoAnimeMv.py
@@ -353,7 +353,7 @@ def Auxiliary_RMOTSTR(File):# 剔除意外字符
     #匹配待去除列表
     FuzzyMatchData = [r'(.*?|=)月新番(.*?|=)',r'\d{4}.\d{2}.\d{2}',r'20\d{2}',r'v[2-9]',r'\d{4}年\d{1,2}月番']
     #精准待去除列表
-    PreciseMatchData = ['仅限港澳台地区','国漫','x264','1080p','720p','4k','\(-\)','（-）']
+    PreciseMatchData = ['仅限港澳台地区','国漫','x264','1080p','720p','4k','\(-\)','（-）','hevc.?10bit',' 10bit','[xh]265.?10bit','[xh]265']
     for i in PreciseMatchData:
         NewPSTRFile = sub(r'%s'%i,'-',NewPSTRFile,flags=I)
     for i in FuzzyMatchData:
